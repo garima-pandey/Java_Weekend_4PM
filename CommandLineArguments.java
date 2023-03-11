3816201190b981649826c1007618f9bd3e2b23a6
Garima Pandey <pgarima148@gmail.com>
2023-03-11 18:01:25 +0530

    Add files via upload

--- CommandLineArguments.java
+++ CommandLineArguments.java
@@ -1,9 +1,13 @@
 class CommandLineArguments {
     public static void main(String[] args) {
-        int firstNumber = Integer.parseInt(args[0]); // type conversion
-        int secondNumber = Integer.parseInt(args[1]);
+        if (args.length == 2) {
+            int firstNumber = Integer.parseInt(args[0]); // type conversion
+            int secondNumber = Integer.parseInt(args[1]);
 
-        int result = firstNumber + secondNumber;
-        System.out.println("Addition is: "+result);
+            int result = firstNumber + secondNumber;
+            System.out.println("Addition is: " + result);
+        } else {
+            System.out.println("Invalid Input");
+        }
     }
 }
---
+++ DataTypes.java
@@ -0,0 +1,11 @@
+class DataTypes {
+    public static void main(String[] args) {
+        // float pi = 3.14f;
+        float pi = (float)3.14;
+        double radius = 2.45;
+        char gender = 'M';
+        boolean status = false;
+        
+        String name = "Garima";
+    }
+}
