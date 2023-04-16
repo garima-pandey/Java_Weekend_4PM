eacad347f5aafc36f39ae50c08797e4d5d2cfefa
Garima Pandey <pgarima148@gmail.com>
2023-04-16 17:47:57 +0530

    Add files via upload

--- src/OOPS/Student.java
+++ src/OOPS/Student.java
@@ -2,21 +2,86 @@ package OOPS;
 
 public class Student {
 	//Instance or Class Variables 
-	int rollno;
-	String name;
-	String phone;
-	String course;
-	double fees;	
+	//private member variables or Data Security
+	private int rollno;
+	private String name;
+	private String phone;
+	private String course;
+	private double fees;
+	private String collegeName;
+	
+	//Default Consturctor
+	Student() {
+		collegeName = "SRCC";
+	}
+	
+	//Parameterized Consturctor
+//	Student(int rollno, String name, String phone, String course, double fees) {
+//		this.rollno = rollno;
+//		this.name = name;
+//		this.phone = phone;
+//		this.course = course;
+//		this.fees = fees;
+//	}
+	
+//	int r, String n, String p, String c, double f - local variables
+//	public void takeInput(int r, String n, String p, String c, double f) {
+//	public void takeInput(int rollno, String name, String phone, String course, double fees) {
+////		rollno = r;
+////		name = n;
+////		phone = p;
+////		course = c;
+////		fees = f;
+//		
+//		//instance variable = local variable
+//		this.rollno = rollno;
+//		this.name = name;
+//		this.phone = phone;
+//		this.course = course;
+//		this.fees = fees;
+//	}
+//	
+	public void print() {
+		System.out.println("Roll no: "+this.rollno);
+		System.out.println("Name: "+name);
+		System.out.println("Phone: "+phone);
+		System.out.println("Course: "+course);
+		System.out.println("Fees: "+fees);
+		System.out.println("College Name: "+collegeName);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Default Constructor Call
 		Student ram = new Student();
-		// ram: reference or address variable
 		
-		System.out.println("Roll no: "+ram.rollno);
-		System.out.println("Name: "+ram.name);
-		System.out.println("Phone: "+ram.phone);
-		System.out.println("Course: "+ram.course);
-		System.out.println("Fees: "+ram.fees);
+		//Parameterized Constructor Call
+//		Student ram = new Student(101, "Ram", "9872638383", "Btech", 10000.0);
+		// ram: reference or address variable
+		ram.print();
+		
+		
+//		ram.takeInput(101, "Ram", "9872638383", "Btech", 10000.0);
+//		System.out.println("*********************");
+//		ram.print();
+		
+//		System.out.println("Roll no: "+ram.rollno);
+//		System.out.println("Name: "+ram.name);
+//		System.out.println("Phone: "+ram.phone);
+//		System.out.println("Course: "+ram.course);
+//		System.out.println("Fees: "+ram.fees);
+		
+//		ram.rollno = 101;
+//		ram.name = "Ram";
+//		ram.phone = "9872638383";
+//		ram.course = "Btech";
+//		ram.fees = 10000.0;
+		
+		
+//		System.out.println("Roll no: "+ram.rollno);
+//		System.out.println("Name: "+ram.name);
+//		System.out.println("Phone: "+ram.phone);
+//		System.out.println("Course: "+ram.course);
+//		System.out.println("Fees: "+ram.fees);
 	}
 }
